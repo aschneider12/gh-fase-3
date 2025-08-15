03e4480b63ed7ad17a1300b9a36507f110a20670
ASchneider <alessandro.as@pc.ms.gov.br>
2025-08-15 14:15:06 -0400

    initial commit

---
+++ src/main/java/br/com/fiap/gh/GhFase3Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhFase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhFase3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/gh/GhFase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhFase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
